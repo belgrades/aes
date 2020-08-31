d351d094acfb39b70fa7727e9019738721445fc2
belgrades <fcremaldc@gmail.com>
2020-08-31 05:23:50 +0000

    Adding test with sum and multiplication

--- tests/word_test.cpp
+++ tests/word_test.cpp
@@ -1,14 +1,17 @@
-#include "../aes/byte.h"
+#include "../aes/unit/byte.h"
 #include<stdio.h>
+#include<iostream>
 
+using namespace std;
 
 int main(){
     Byte a(0x57), b(0x83);
-    Byte c;
+    Byte c, d;
 
     c = a + b;
+    d = a * b;
 
-    printf("0x%x ", c.value);        
-    
+    cout << c << endl;   
+    cout << d << endl;
     return 0;
 }
